b9fb73a2d9f9c45e6e5732b0bb2b75bda42b5f6d
DragonSlayer64 <e08lf1@svmms.se>
2023-02-06 20:43:43 +0100

    creating header

--- booking/src/App.js
+++ booking/src/App.js
@@ -1,11 +1,16 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
+import List from "./pages/home/list/List";
+import Hotel from "./pages/home/hotels/Hotel";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/hotels" element={<List />}></Route>
+        <Route exact path="/hotels/:id" element={<Hotel />}></Route>
       </Routes>
     </BrowserRouter>
   );
